43a22289233d4f2250923bc306d86adfabed31cb
Rachel Cynthia V <rachel883omega@gmail.com>
2025-07-09 21:55:49 +0100

    Initial prototype

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ImageViewer from '../components/ImageViewer/ImageViewer';
+import MapWidget from '../components/MapWidget/MapWidget';
+
+function App() {
+  return (
+    <ImageViewer />
+  );
+}
+
+export default App;
